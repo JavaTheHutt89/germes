5dbe25badb01e223bcde9283ab7f12a7aefd68ec
Юровник Максим Васильевич <21481@7733.ru>
2018-09-24 17:17:11 +1000

    http://it-simulator.com, learning full-stack development

--- germesservice/src/main/java/org/yurovnik/germes/app/rest/service/transform/impl/SimpleDTOTransformer.java
+++ germesservice/src/main/java/org/yurovnik/germes/app/rest/service/transform/impl/SimpleDTOTransformer.java
@@ -1,5 +1,9 @@
 package org.yurovnik.germes.app.rest.service.transform.impl;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yurovnik.germes.app.infra.util.Checks;
 import org.yurovnik.germes.app.infra.util.ReflectionUtil;
 import org.yurovnik.germes.app.model.entity.base.AbstractEntity;
@@ -7,12 +11,19 @@ import org.yurovnik.germes.app.rest.dto.base.BaseDTO;
 import org.yurovnik.germes.app.rest.service.transform.Transformer;
 
 public class SimpleDTOTransformer implements Transformer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
+
     @Override
     public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(T entity, Class<P> clz) {
         checkParams(entity, clz);
         P dto = ReflectionUtil.createInstance(clz);
         ReflectionUtil.copyFields(entity, dto, ReflectionUtil.findSimilarFields(entity.getClass(), clz));
         dto.transform(entity);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.transform: {} DTO object", toString(dto));
+        }
+
         return dto;
     }
 
@@ -27,6 +38,15 @@ public class SimpleDTOTransformer implements Transformer {
         T entity = ReflectionUtil.createInstance(clz);
         ReflectionUtil.copyFields(dto, entity, ReflectionUtil.findSimilarFields(dto.getClass(), clz));
         dto.unTransform(entity);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("SimpleDTOTransformer.transform: {} entity", toString(entity));
+        }
+
         return entity;
+    }
+
+    public static String toString(Object param){
+        return ReflectionToStringBuilder.toString(param, ToStringStyle.SHORT_PREFIX_STYLE);
     }
 }
